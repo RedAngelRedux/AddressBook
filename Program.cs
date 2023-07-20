64dfac042a8749964b878c9001340c0b84843609
RedAngelRedux <sammy@sammynava.com>
2023-07-20 16:53:47 -0700

    Add support for local postgres database migrate DB defined in .NET to postgres, enabling our code-first approach to schema creation.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
+using AddressBook.Data;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
