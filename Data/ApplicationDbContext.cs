3c18809773d29f24e41f1547af2e1a1d1ab9dbe5
RedAngelRedux <sammy@sammynava.com>
2023-07-20 17:28:22 -0700

    Create Contact Model and perform Initial migration to create a new Contacts table in postgres.

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AddressBook.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AddressBook.Data
 {
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Contact> Contacts { get; set; }
     }
+
+
 }
