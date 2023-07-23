a43ae6a6112a7bdf39cf0b6114d28cd53fd5fc9c
RedAngelRedux <sammy@sammynava.com>
2023-07-22 19:55:33 -0700

    Add ability to upload and save an image file to the database

--- Models/Contact.cs
+++ Models/Contact.cs
@@ -7,41 +7,45 @@ namespace AddressBook.Models
     {
         [Required]
         [Display(Name ="First Name")]
-        public string FirstName { get; set; }
+        public string FirstName { get; set; } = string.Empty;
 
         [Required]
-        [Display(Name ="Last Name")]
-        public string LastName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; } = string.Empty;
 
         [Required]
-        public string Address1 { get; set; }
+        public string Address1 { get; set; } = string.Empty;
 
-        public string Address2 { get; set; }
+        public string? Address2 { get; set; }
 
-        public string City { get; set; }
+        [Required]
+        public string City { get; set; } = string.Empty;
 
-        public string State { get; set; }
-        
+        [Required]
+        public string State { get; set; } = string.Empty;
+
+        [Required]
         [DataType(DataType.PostalCode)]
         public int Zip { get; set; }
 
         [DataType(DataType.EmailAddress)]
-        public string Email { get; set; }
+        public string? Email { get; set; }
 
         [DataType(DataType.PhoneNumber)]
-        public string Phone { get; set; }
+        public string? Phone { get; set; }
 
-        public DateTime Created { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
 
         [NotMapped]
         [Display(Name = "Image")]
         [DataType(DataType.Upload)]
-        public IFormFile ImageFile { get; set; }
+        public IFormFile? ImageFile { get; set; }
 
-        public byte[] ImageData { get; set; }
+        public byte[]? ImageData { get; set; }
 
-        public string ImageType { get; set; }
+        public string? ImageType { get; set; }
 
+        [Key]
         public int Id { get; set; }
         
         [NotMapped]
